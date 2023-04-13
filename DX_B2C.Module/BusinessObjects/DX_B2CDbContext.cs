d91d7885a2900016e0f433655c9bf51e1005d32f
igomez <igomez@soaint.com>
2023-04-13 01:17:49 -0500

    first upload

---
+++ DX_B2C.Module/BusinessObjects/DX_B2CDbContext.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace DX_B2C.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class DX_B2CContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<DX_B2CEFCoreDbContext>()
+            .UseSqlServer(";");
+        return new DX_B2CEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class DX_B2CDesignTimeDbContextFactory : IDesignTimeDbContextFactory<DX_B2CEFCoreDbContext> {
+	public DX_B2CEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<DX_B2CEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DX_B2C");
+		//return new DX_B2CEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(DX_B2CContextInitializer))]
+public class DX_B2CEFCoreDbContext : DbContext {
+	public DX_B2CEFCoreDbContext(DbContextOptions<DX_B2CEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<DX_B2C.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<DX_B2C.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+	public DbSet<FileData> FileData { get; set; }
+	public DbSet<ReportDataV2> ReportDataV2 { get; set; }
+	public DbSet<DashboardData> DashboardData { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<DX_B2C.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+    }
+}
